817caf75fc22cfa5071a99c2ad3640bc96f84aea
marcusbey <rboboe@gmail.com>
2020-03-09 16:42:21 -0400

    adding individial post

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView 
 from .models import Post
 
 class BlogListView(ListView):
 	model = Post
 	template_name = 'home.html'
+
+class BlogDetailView(DetailView): # new model = Post
+	model = Post
+	template_name = 'post_detail.html'
