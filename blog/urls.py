47b50df19796aa16972c6532ea32fee50ab54b39
marcusbey <rboboe@gmail.com>
2020-03-09 23:38:32 -0400

    Adding delete fonciton et text update

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,13 @@ from .views import (
 	BlogDetailView, 
 	BlogCreateView, 
 	BlogUpdateView,
+	BlogDeleteView,
 	)
 
 urlpatterns = [
- 	path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
-	path('post_new/', BlogCreateView.as_view(), name='post_new'),
+	path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+ 	path('post_new/', BlogCreateView.as_view(), name='post_new'),
 	path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+	path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
 	path('', BlogListView.as_view(), name='home'),
 ]
